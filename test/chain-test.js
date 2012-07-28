70f3dac05233dd6221662f8512f3cf70fc79f300
Tyler Roscoe <tyler@cryptio.net>
2012-07-28 01:16:38 -0700

    slightly less trivial test. a suggested :map invocation for vim.

--- test/chain-test.js
+++ test/chain-test.js
@@ -1,9 +1,10 @@
-// Node tests
 var buster = require("buster");
 var myLib = require("../chain.js");
 
-buster.testCase("A module", {
-    "states the obvious": function () {
-        assert(true);
+buster.testCase("trackFitsChain", {
+    "is trivially true": function () {
+        assert(global.trackFitsChain("test_track", "test_chain"));
     }
 });
+
+// :map T :!node test/chain-test.js<CR>
