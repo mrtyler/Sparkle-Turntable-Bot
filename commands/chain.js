c4c15f98a2d839efd085892b927556c728e15f1d
Tyler Roscoe <tyler@cryptio.net>
2012-07-28 00:07:41 -0700

    those setTimeouts were probably there for a reason.

--- commands/chain.js
+++ commands/chain.js
@@ -3,10 +3,8 @@ exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data) {
-    var response = ('Current chain:' + global.chain);
-    output({text: response, destination: data.source, userid: data.userid});
-//    setTimeout(function() {
-//        var response = (config.responses.rules.description);
-//        output({text: response, destination: data.source, userid: data.userid});
-//    }, 600);
+    setTimeout(function() {
+        var response = ('Current chain:' + global.chain);
+        output({text: response, destination: data.source, userid: data.userid});
+    }, 600);
 }
--- commands/setchain.js
+++ commands/setchain.js
@@ -3,11 +3,9 @@ exports.hidden = true;
 exports.enabled = true;
 exports.matchStart = true;
 exports.handler = function(data) {
-    global.chain = data.text.substring(exports.name.length + 1);
-    var response = ('Chain set to:' + global.chain);
-    output({text: response, destination: data.source, userid: data.userid});
-//    setTimeout(function() {
-//        var response = (config.responses.rules.description);
-//        output({text: response, destination: data.source, userid: data.userid});
-//    }, 600);
+    setTimeout(function() {
+        global.chain = data.text.substring(exports.name.length + 1);
+        var response = ('Chain set to:' + global.chain);
+        output({text: response, destination: data.source, userid: data.userid});
+    }, 600);
 }
