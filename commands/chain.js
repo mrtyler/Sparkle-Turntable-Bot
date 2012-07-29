2e27020d7c416e4827feb5ebd6761dab8aa28573
Tyler Roscoe <tyler@cryptio.net>
2012-07-28 21:33:14 -0700

    More subcommands.

--- commands/chain.js
+++ commands/chain.js
@@ -8,14 +8,30 @@ exports.handler = function(data) {
         var reply = 'default (should never be displayed)';
 
         if (/^set /.test(subcommand)) {
+            global.previous_chain = global.chain;
             global.chain = subcommand.substring('set '.length);
-            reply = 'Setting chain to: ' + global.chain;
-        } else if(/^before /.test(subcommand)) {
-            global.chain = subcommand.substring('before '.length) + ' ' + global.chain;
-            reply = 'Prepended. Chain is now: ' + global.chain;
-        } else if(/^after /.test(subcommand)) {
-            global.chain = global.chain + ' ' + subcommand.substring('after '.length);
-            reply = 'Appended. Chain is now: ' + global.chain;
+            reply = 'Set chain to: ' + global.chain;
+        } else if(/^revert/.test(subcommand)) {
+            var replaced = global.chain;
+            global.chain = global.previous_chain;
+            global.previous_chain = replaced;
+            reply = 'Revert chain to: ' + global.chain;
+        } else if(/^before/.test(subcommand)) {
+            global.previous_chain = global.chain;
+            global.chain = (currentsong.song || '') + ' ' + global.chain;
+            reply = 'Before. Chain is now: ' + global.chain;
+        } else if(/^after/.test(subcommand)) {
+            global.previous_chain = global.chain;
+            global.chain = global.chain + ' ' + (currentsong.song || '');
+            reply = 'After. Chain is now: ' + global.chain;
+        } else if(/^prepend /.test(subcommand)) {
+            global.previous_chain = global.chain;
+            global.chain = subcommand.substring('prepend '.length) + ' ' + global.chain;
+            reply = 'Prepend. Chain is now: ' + global.chain;
+        } else if(/^append /.test(subcommand)) {
+            global.previous_chain = global.chain;
+            global.chain = global.chain + ' ' + subcommand.substring('append '.length);
+            reply = 'Append. Chain is now: ' + global.chain;
         } else {
             reply = 'Current chain: ' + global.chain;
         }
